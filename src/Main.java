aec18281b432a9047d3cce4a59fc9c0a8c3c20d6
amrutaparande <amruta.parande09@gmail.com>
2023-05-30 19:19:20 -0600

    Minor change

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         for(int i=0;i<10;i++){
-        System.out.println("Hello world!");
+        System.out.println("Hello world2!");
         Thread.sleep(2000);
         }
     }
